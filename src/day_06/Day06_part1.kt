e03bdd0cac458e29bb75e2d2c5a2898263a2c961
Miguel Alpizar <maac.1106@gmail.com>
2021-12-07 14:13:40 -0600

    Solve day 6 part 1

---
+++ src/day_06/Day06_part1.kt
@@ -0,0 +1,25 @@
+package day_06
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val numbers = input.first().split(",").map { it.toInt() }.toMutableList()
+        for (i in 1..80) {
+            (0 until numbers.size).forEach { index ->
+                if (numbers[index] == 0) {
+                    numbers[index] = 7
+                    numbers.add(8)
+                }
+                numbers[index] -= 1
+            }
+        }
+        return numbers.size
+    }
+
+    val testInput = readInput("day_06/Input_test")
+    check(part1(testInput) == 5934)
+
+    val input = readInput("day_06/Input")
+    println(part1(input))
+}

e2c635f3b1f17b279a9d732a1784e93a473703a3
Miguel Alpizar <maac.1106@gmail.com>
2021-12-13 00:42:06 -0600

    Improve day 5 part 1

--- src/day_05/Day05_part1.kt
+++ src/day_05/Day05_part1.kt
@@ -3,11 +3,9 @@ package day_05
 import readInput
 
 fun main() {
-    fun isHorizontal(ranges: List<Int>) = ranges[1] == ranges[3]
+    fun isNotDiagonal(ranges: List<Int>) = ranges[0] == ranges[2] || ranges[1] == ranges[3]
 
-    fun isVertical(ranges: List<Int>) = ranges[0] == ranges[2]
-
-    fun getRange(first: Int, second: Int) = if (first < second) first..second else second..first
+    fun getRange(first: Int, second: Int) = if (first < second) first.rangeTo(second) else first.downTo(second)
 
     fun part1(input: List<String>): Int {
         val diagram = arrayListOf<Pair<Int, Int>>()
@@ -16,22 +14,17 @@ fun main() {
                     .replace("->", ",")
                     .split(",")
                     .map { number -> number.toInt() }
-            when {
-                isHorizontal(ranges) -> {
-                    val y = ranges[1]
-                    for (x in getRange(ranges[0], ranges[2])) {
-                        diagram.add(Pair(x, y))
-                    }
-                }
-                isVertical(ranges) -> {
-                    val x = ranges[0]
-                    for (y in getRange(ranges[1], ranges[3])) {
+            if (isNotDiagonal(ranges)) {
+                val xRange = getRange(ranges[0], ranges[2])
+                val yRange = getRange(ranges[1], ranges[3])
+                for (x in xRange) {
+                    for (y in yRange) {
                         diagram.add(Pair(x, y))
                     }
                 }
             }
         }
-        return diagram.groupBy { Pair(it.first, it.second) }.filter { it.value.size > 1 }.size
+        return diagram.groupBy { it }.filter { it.value.size > 1 }.size
     }
 
     val testInput = readInput("day_05/Input_test")
